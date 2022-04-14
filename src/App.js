a1fd900fbb1a37682b71a642d3f1185e9cffb873
musaliev <umusaliev28@gmail.com>
2022-04-14 15:40:50 +0600

    added Contact

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div>
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
